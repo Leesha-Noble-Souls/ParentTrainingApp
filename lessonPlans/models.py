3a19138ee2579468900f23b0aa046a480ed1570c
Avishi Gupta <avishig012@gmail.com>
2021-02-04 00:11:38 +0530

    Add LessonPlan model and create admin

--- lessonPlans/models.py
+++ lessonPlans/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+VISIBLE = True
+DONE = True
+RECEIVED = True
+
+class LessonPlan(models.Model):
+    topic = models.CharField(max_length = 200)
+    aim = models.CharField(max_length = 200, default = "Aim:")
+    objectives = models.TextField(null = True)
+    activity = models.TextField()
+    link = models.URLField(null = True)
+    visibility = models.BooleanField(default = not VISIBLE)
+    lesson_status = models.BooleanField(default = not DONE)
+    feedback_status = models.BooleanField(default = not RECEIVED)
+
+    def __str__(self):
+        return self.topic
--- parentTraining/settings.py
+++ parentTraining/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lessonPlans.apps.LessonplansConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
