f16c73992b982a5ef26096a5c10b6528496c0471
Avishi Gupta <avishig012@gmail.com>
2021-02-16 09:24:29 +0530

    Set up home page URL

--- parentTraining/urls.py
+++ parentTraining/urls.py
@@ -19,7 +19,10 @@ from django.contrib.staticfiles import views
 from django.contrib import admin
 from django.urls import include, path, re_path
 
+from . import views
+
 urlpatterns = [
+    path('', views.home, name='home'),
     path('lessonPlans/', include('lessonPlans.urls')),
     path('admin/', admin.site.urls),
 ]
