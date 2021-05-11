e7261a1a34fd257daaa8ce0f06c27292ed34447e
Shubham Mehra <subhammehra4@gmail.com>
2021-05-11 18:47:20 +0530

    Variable Refactor

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,12 +6,12 @@ from lessonPlans.models import LessonPlan
 @login_required
 def dashboard(request):
     current_user = request.user
-    available_plans = {}
+    topics_to_plans = {}
 
     for plan in LessonPlan.objects.all():
         if current_user in plan.assigned_users.all():
             for topic in plan.topics.all() :
-                available_plans.setdefault(topic, []).append({"id" : plan.id, "title": plan.title, "aim":plan.aim})
-                # available_plans.setdefault(topic, []).append({"id" : plan.id, "title": plan.title, "aim":plan.aim, "topics" : plan.topics.all()})
+                topics_to_plans.setdefault(topic, []).append({"id" : plan.id, "title": plan.title, "aim":plan.aim})
+                # topics_to_plans.setdefault(topic, []).append({"id" : plan.id, "title": plan.title, "aim":plan.aim, "topics" : plan.topics.all()})
         
     return render(request, "dashboard/index.html", {"available_plans" : available_plans})
