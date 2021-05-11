95dcaf89ae5242d7e44264fb531f8b7fb6243359
Shubham Mehra <subhammehra4@gmail.com>
2021-05-11 18:43:36 +0530

    Refactor 2

--- dashboard/views.py
+++ dashboard/views.py
@@ -11,6 +11,7 @@ def dashboard(request):
     for plan in LessonPlan.objects.all():
         if current_user in plan.assigned_users.all():
             for topic in plan.topics.all() :
-                available_plans.setdefault(topic, []).append({"id" : plan.id, "title": plan.title, "aim":plan.aim, "topics" : plan.topics.all()})
+                available_plans.setdefault(topic, []).append({"id" : plan.id, "title": plan.title, "aim":plan.aim})
+                # available_plans.setdefault(topic, []).append({"id" : plan.id, "title": plan.title, "aim":plan.aim, "topics" : plan.topics.all()})
         
     return render(request, "dashboard/index.html", {"available_plans" : available_plans})
