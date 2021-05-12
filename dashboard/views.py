3837d9af69ca75927a64ae87100b128b0dad0573
Shubham Mehra <subhammehra4@gmail.com>
2021-05-12 11:06:01 +0530

    Refactor_4

--- dashboard/views.py
+++ dashboard/views.py
@@ -14,4 +14,4 @@ def dashboard(request):
                 topic_to_plans.setdefault(topic, []).append({"id" : plan.id, "title": plan.title, "aim":plan.aim})
                 # topic_to_plans.setdefault(topic, []).append({"id" : plan.id, "title": plan.title, "aim":plan.aim, "topics" : plan.topics.all()})
         
-    return render(request, "dashboard/index.html", {"available_plans" : available_plans})
+    return render(request, "dashboard/index.html", {"topic_to_plans" : topic_to_plans})
