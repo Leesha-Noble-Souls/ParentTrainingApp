3fd03506cf957addcbe4b77f391827cf57b59af7
s-gup <shelly19108@iiitd.ac.in>
2021-11-14 17:01:09 +0530

    favicon add

--- lessonPlans/admin.py
+++ lessonPlans/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from .models import LessonPlan, Topic, LessonFeedback,OnlineTraining,ShortCourses,Webinar
-
+admin.site.site_header = 'Vyom Self Help Foundation'
 admin.site.register(Topic)
 admin.site.register(LessonPlan)
 admin.site.register(LessonFeedback)
